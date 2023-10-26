94c70476bcd7a294302b6a3a1555d54314285cd0
Sohan <sohan.j12@gmail.com>
2023-10-26 17:48:09 +0530

    added to-be-fixed issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { RecoilRoot } from 'recoil'
-import {InitUser} from '../components/InitUser'
-import Appbar from '../components/Appbar'
-import Footer from '@/components/Footer'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+import { InitUser } from "../components/InitUser";
+import Appbar from "../components/Appbar";
+import Footer from "@/components/Footer";
 
+//fix main page padding later
 export default function App({ Component, pageProps }: AppProps) {
-  return <RecoilRoot>
-    <div style={{display: 'flex',
-  flexDirection: 'column',
-  minHeight: '100vh', }}>
-    <Appbar/>
-    <InitUser/>
-    <Component {...pageProps} />
-    <Footer/>
-    </div>
-  </RecoilRoot>
+  return (
+    <RecoilRoot>
+      <div
+        style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
+      >
+        <Appbar />
+        <InitUser />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </RecoilRoot>
+  );
 }
